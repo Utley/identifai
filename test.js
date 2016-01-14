b26d78b5c25b97b3c25e192b326603be28757e0e
utley <utley2600@gmail.com>
2016-01-14 13:09:44 -0500

    tag multiple images in array

--- test.js
+++ test.js
@@ -2,4 +2,24 @@ var Clarifai = require('./clarifai_node.js');
 
 Clarifai.initAPI(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
-
+testURLs = ['https://octodex.github.com/images/labtocat.png',
+	    		'https://octodex.github.com/images/octoliberty.png'];
+testIDs = ['lab','liberty','robot'];
+Clarifai.tagURL(testURLs, testIDs, function(err, res){
+	if(err){
+		console.log(err);
+	}
+	else{
+	  if(typeof res["status_code"] === "string" && res["status_code"] === "OK"){
+			for(var i in res.results){
+			  var tags = res.results[i].result.tag;
+				console.log();
+				console.log("Image: " + res.results[i].local_id);
+				console.log();
+			  for(var j in tags.classes){
+			    console.log(tags.classes[j] + ': ' + tags.probs[j]);
+			  }
+			}
+	  }
+	}
+});
